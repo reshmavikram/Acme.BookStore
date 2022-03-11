ad23c2a3bac19f491d60feef5170f31546c45e06
reshmavikram <reshmavikramraj@gmail.com>
2022-03-11 12:02:55 +0530

    Add project files.

---
+++ src/Acme.BookStore.Domain/Books/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Books
+{
+   
+    public class User : AuditedAggregateRoot<Guid>
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+
+        //Navigation
+        public virtual UserAddress? Address { get; set; }
+
+    }
+}
---
+++ src/Acme.BookStore.Domain/Books/UserAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Books
+{
+    public class UserAddress : AuditedAggregateRoot<Guid>
+    {
+        public Guid Id { get; set; }
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+
+        //NAVIGATION
+        public Guid? UserId { get; set; }//FK
+        public virtual User? User { get; set; }
+
+    }
+}
